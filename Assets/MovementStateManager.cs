bef302677905aae58cb6cfe62fed7f33d961dd09
kfa802 <147632358+kfa802@users.noreply.github.com>
2026-02-26 13:03:28 +0100

    fix gizmo

--- Assets/MovementStateManager.cs
+++ Assets/MovementStateManager.cs
@@ -130,9 +130,9 @@ public class MovementStateManager : MonoBehaviour
         controller.Move(velocity * Time.deltaTime);
     }
 
-    private void OnDrawGizmos()
+    /*private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(spherePos, controller.radius - 0.05f);
-    }
+    }*/
 }
